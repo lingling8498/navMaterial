427b9223304ca0eb1ffb1bc3ffd1ea26011567f0
lingling8498 <Ling.Wang@WolfDen.wolfmedical.com>
2018-09-06 14:49:16 -0700

    navMaterial - Angular IO Tutorial Guide - May 23 2018

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
